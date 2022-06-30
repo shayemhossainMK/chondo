6c222757864a6275a4efe4612474314c58c9f59e
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-06-30 20:36:37 +0600

    new codae added

--- src/pages/Home/HomeBlogContainer.js
+++ src/pages/Home/HomeBlogContainer.js
@@ -1,20 +1,28 @@
 import { faEye, faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const HomeBlogContainer = ({ blog }) => {
-  const { name, img, text, like, views } = blog;
+  const navigate = useNavigate();
+  const { _id, name, img, text, like, views } = blog;
+
+  const navigateBlogDetails = () => {
+    navigate(`/blogdetails/${_id}`);
+  };
+
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:w-[750px] mx-auto mt-5 mb-20 shadow-xl drop-shadow-md rounded-2xl p-5">
         <div className="col-span-2 order-2 md:order-1">
           <div>
-            <Link to="/blogdetails">
-              <h1 className="text-3xl font-bold mt-1 hover:underline">
-                {name}
-              </h1>
-            </Link>
+            <h1
+              onClick={() => navigateBlogDetails(_id)}
+              className="text-3xl font-bold mt-1 hover:underline"
+            >
+              {name}
+            </h1>
+
             <p className="mt-1">{text}</p>
             <div className="flex mt-2">
               <p className="mr-4">
